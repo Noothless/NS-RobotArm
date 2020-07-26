88af4ca01a04c3b4208776586a079c64ffa72596
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-07-26 21:14:51 +0200

    Update detectnet.py

--- jetson-inference/detectnet.py
+++ jetson-inference/detectnet.py
@@ -3,7 +3,7 @@ import jetson.inference
 import jetson.utils
 
 net = jetson.inference.detectNet("coco-bottle", threshold=0.5)
-camera = jetson.utils.videoSource("rtsp://192.168.1.171", codec=h264)   
+camera = jetson.utils.videoSource("rtsp://192.168.1.171")   
 display = jetson.utils.videoOutput("rtp://192.168.1.144:5000") 
 
 while display.IsStreaming():
