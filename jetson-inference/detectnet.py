46d2e1b27a07edda8b6216922d5a4164735b8c77
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-07-26 21:22:45 +0200

    Update detectnet.py

--- jetson-inference/detectnet.py
+++ jetson-inference/detectnet.py
@@ -2,16 +2,46 @@
 import jetson.inference
 import jetson.utils
 
-net = jetson.inference.detectNet("coco-bottle", threshold=0.5)
-camera = jetson.utils.videoSource("rtsp://192.168.1.171")   
-display = jetson.utils.videoOutput("rtp://192.168.1.144:5000") 
+import argparse
+import sys
 
-while display.IsStreaming():
-	img = camera.Capture()
-	detections = net.Detect(img)
-	display.Render(img)
+# parse the command line
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
+                                 jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
+
+parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
+parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
+
+is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
+
+try:
+	opt = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
+
+input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
+
+while True:
+	img = input.Capture()
+	detections = net.Detect(img, overlay=opt.overlay)
 
 	for detection in detections:
 		print(detection)
 
-	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+	output.Render(img)
+
+	output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
+
+	net.PrintProfilerTimes()
+
+	if not input.IsStreaming() or not output.IsStreaming():
+		break
