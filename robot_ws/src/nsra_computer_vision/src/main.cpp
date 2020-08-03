dafdca05d6eb833c960d616a2f6437c40bdcc695
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-03 11:05:07 +0200

    opencv

--- robot_ws/src/nsra_computer_vision/src/main.cpp
+++ robot_ws/src/nsra_computer_vision/src/main.cpp
@@ -231,8 +231,8 @@ int main(int argc, char* argv[])
 {
     help();
     Settings s;
-    string path = ros::package::getPath("nsra_computer_vision") + "/default.xml";
-    const string inputSettingsFile = argc > 1 ? argv[1] : path;
+    string path = ros::package::getPath("nsra_computer_vision");
+    const string inputSettingsFile = argc > 1 ? argv[1] : "default.xml";
     FileStorage fs(inputSettingsFile, FileStorage::READ); // Read the settings
     if (!fs.isOpened())
     {
