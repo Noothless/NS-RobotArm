3993824a6cbc31ec8142917e6c4d25530b3c7204
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-07 00:10:54 +0200

    Update read_images.cpp

--- robot_ws/src/nsra_computer_vision/src/read_images.cpp
+++ robot_ws/src/nsra_computer_vision/src/read_images.cpp
@@ -23,7 +23,7 @@ void imgSaveCallback(const std_msgs::String::ConstPtr& msg) {
   imwrite(filename1, img1);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
   char* cam_address;
   char* name;
