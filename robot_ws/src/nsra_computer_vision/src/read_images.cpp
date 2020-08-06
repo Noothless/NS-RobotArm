4da3f359db9c3dfd24fa6ee5d43c3e4c653730e6
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-06 17:40:57 +0200

    cpp thread

--- robot_ws/src/nsra_computer_vision/src/read_images.cpp
+++ robot_ws/src/nsra_computer_vision/src/read_images.cpp
@@ -4,12 +4,14 @@
 #include <stdio.h>
 #include <iostream>
 #include "popt_pp.h"
+#include <thread> 
 
 using namespace std;
 using namespace cv;
 
 int x = 0;
 bool k = true;
+bool t = true;
 
 int main(int argc, char const *argv[])
 {
@@ -29,35 +31,51 @@ int main(int argc, char const *argv[])
   POpt popt(NULL, argc, argv, options, 0);
   int c;
   while((c = popt.getNextOpt()) >= 0) {}
+  
+  std::thread thread1(cam1); 
+  std::thread thread2(cam2); 
+  
+  return 0;
+}
 
+void cam1(){
   VideoCapture cap1("rtsp://192.168.1.171");
-  VideoCapture cap2("rtsp://192.168.1.190");
-  Mat img1, img_res1, img2, img_res2;
+  Mat img1, img_res1;
   while (1) {
     cap1 >> img1;
-    //cap2 >> img2;
     resize(img1, img_res1, Size(im_width, im_height));
-    //resize(img2, img_res2, Size(im_width, im_height));
     imshow("IMG1", img1);
-    //imshow("IMG2", img2);
     int key = waitKey(50);
     if ((key != 255) && k) {
       x++;
-      char filename1[200];//, filename2[200];
+      char filename1[200];
       sprintf(filename1, "%sleft%d.%s", imgs_directory, x, extension);
-      //sprintf(filename2, "%sright%d.%s", imgs_directory, x, extension);
       cout << "Saving img pair " << x << endl;
       imwrite(filename1, img1);
-      //imwrite(filename2, img2);
       k = false;
     } else if (key == 255){
       k = true;
     }
   }
-  return 0;
 }
-/*
-void cam1(VideoCapture cap){
 
+void cam1(){
+  VideoCapture cap2("rtsp://192.168.1.190");
+  Mat img2, img_res2;
+  while (1) {
+    cap2 >> img2;
+    resize(img2, img_res2, Size(im_width, im_height));
+    imshow("IMG1", img2);
+    int key = waitKey(50);
+    if ((key != 255) && t) {
+      x++;
+      char filename2[200];
+      sprintf(filename2, "%sleft%d.%s", imgs_directory, x, extension);
+      cout << "Saving img pair " << x << endl;
+      imwrite(filename2, img2);
+      t = false;
+    } else if (key == 255){
+      t = true;
+    }
+  }
 }
-*/
