78a9a96b303372eec8ce947a41ea5e7c9d9cd89b
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-06 13:20:03 +0200

    Update read_images.cpp

--- robot_ws/src/nsra_computer_vision/src/read_images.cpp
+++ robot_ws/src/nsra_computer_vision/src/read_images.cpp
@@ -30,23 +30,23 @@ int main(int argc, char const *argv[])
   while((c = popt.getNextOpt()) >= 0) {}
 
   VideoCapture cap1("rtsp://192.168.1.105");
-  //VideoCapture cap2("rtsp://192.168.1.105");
-  Mat img1, img_res1;//, img2, img_res2;
+  VideoCapture cap2("rtsp://192.168.1.171");
+  Mat img1, img_res1, img2, img_res2;
   while (1) {
     cap1 >> img1;
-    //cap2 >> img2;
+    cap2 >> img2;
     resize(img1, img_res1, Size(im_width, im_height));
-    //resize(img2, img_res2, Size(im_width, im_height));
+    resize(img2, img_res2, Size(im_width, im_height));
     imshow("IMG1", img_res1);
-    //imshow("IMG2", img_res2);
-    if (waitKey(30) > 0) {
+    imshow("IMG2", img_res2);
+    if (waitKey(200) > 0) {
       x++;
-      char filename1[200];//, filename2[200];
+      char filename1[200], filename2[200];
       sprintf(filename1, "%sleft%d.%s", imgs_directory, x, extension);
-      //sprintf(filename2, "%sright%d.%s", imgs_directory, x, extension);
+      sprintf(filename2, "%sright%d.%s", imgs_directory, x, extension);
       cout << "Saving img pair " << x << endl;
       imwrite(filename1, img_res1);
-      //imwrite(filename2, img_res2);
+      imwrite(filename2, img_res2);
     }
   }
   return 0;
