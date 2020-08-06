27da2a08b856798efd93a2674483edeb847bb5be
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-07 00:47:23 +0200

    Update read_images.cpp

--- robot_ws/src/nsra_computer_vision/src/read_images.cpp
+++ robot_ws/src/nsra_computer_vision/src/read_images.cpp
@@ -4,16 +4,18 @@
 #include <stdio.h>
 #include <iostream>
 #include "popt_pp.h"
-//#include "ros/ros.h"
-//#include "std_msgs/String.h"
-//#include <sstream>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
 
 using namespace std;
 using namespace cv;
 
 int x = 0;
+Mat img1, img_res1;
+char* imgs_directory;
+char* extension;
 
-/*
 void imgSaveCallback(const std_msgs::String::ConstPtr& msg) {
   x++;
   char filename1[200];
@@ -21,12 +23,11 @@ void imgSaveCallback(const std_msgs::String::ConstPtr& msg) {
   cout << "Saving img pair " << x << endl;
   imwrite(filename1, img1);
 }
-*/
 
-int main(int argc, char const *argv[])
+int main(int argc, char** argv)
 {
-  char* imgs_directory;
-  char* extension;
+  char* cam_address;
+  char* name;
   int im_width, im_height;
 
   static struct poptOption options[] = {
@@ -34,6 +35,8 @@ int main(int argc, char const *argv[])
     { "img_height",'h',POPT_ARG_INT,&im_height,0,"Image height","NUM" },
     { "imgs_directory",'d',POPT_ARG_STRING,&imgs_directory,0,"Directory to save images in","STR" },
     { "extension",'e',POPT_ARG_STRING,&extension,0,"Image extension","STR" },
+    { "cam_address",'c',POPT_ARG_STRING,&cam_address,0,"RTSP Camera Address","STR" },
+    { "name",'n',POPT_ARG_STRING,&name,0,"Camera Name","STR" },
     POPT_AUTOHELP
     { NULL, 0, 0, NULL, 0, NULL, NULL }
   };
@@ -42,29 +45,30 @@ int main(int argc, char const *argv[])
   int c;
   while((c = popt.getNextOpt()) >= 0) {}
 
-  //const char *const_name = name;
-  //ros::init(argc, argv, const_name);
-  //ros::NodeHandle n;
+  const char *const_name = name;
+  ros::init(argc, argv, const_name);
+  ros::NodeHandle n;
 
-  //ros::Subscriber sub = n.subscribe("save_img", 1000, imgSaveCallback);
+  ros::Subscriber sub = n.subscribe("save_img", 1000, imgSaveCallback);
 
-  VideoCapture cap1("rtsp://192.168.1.171");
+  VideoCapture cap1(cam_address);
 
   cap1.set(CAP_PROP_BUFFERSIZE, 2);
   //ros::Rate r(100);
-  Mat img1, img_res1;
+
   while (1) {
     cap1 >> img1;
     resize(img1, img_res1, Size(im_width, im_height));
-    imshow("IMG1", img_res1);
-    //ros::spinOnce();
-    //r.sleep();
+    imshow(name, img_res1);
     if (waitKey(30) != 255) {
       x++;
       char filename1[200];
       sprintf(filename1, "%sleft%d.%s", imgs_directory, x, extension);
       cout << "Saving img pair " << x << endl;
+      imwrite(filename1, img_res1);
     }
+    ros::spinOnce();
+    //r.sleep();
   }
   return 0;
 }
