dcacff9562d3b60015076b3e4d6d129fe09f9e8a
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-06 19:38:06 +0200

    Update read_images.cpp

--- robot_ws/src/nsra_computer_vision/src/read_images.cpp
+++ robot_ws/src/nsra_computer_vision/src/read_images.cpp
@@ -4,52 +4,23 @@
 #include <stdio.h>
 #include <iostream>
 #include "popt_pp.h"
-#include <thread> 
-#include <mutex>
 
 using namespace std;
 using namespace cv;
 
 int x = 0;
-bool t1 = true;
-bool t2 = true;
-bool t = true;
-Mat img1, img_res1, img2, img_res2;
-mutex m1;
-mutex m2;
-
-VideoCapture cap1("rtsp://192.168.1.171");
-VideoCapture cap2("rtsp://192.168.1.190");
-
-void cam1(char* imgs_directory, char* extension, int im_width, int im_height){
-  while(1){
-    m1.lock();
-    cap1 >> img1;
-    m1.unlock();
-  }
-}
-
-void cam2(char* imgs_directory, char* extension, int im_width, int im_height){
-  while(1){
-    m2.lock();
-    cap2 >> img2;
-    m2.unlock();
-  }
-}
-
-
 
 int main(int argc, char const *argv[])
 {
-  char* imgs_directory_arg;
-  char* extension_arg;
-  int im_width_arg, im_height_arg;
+  char* imgs_directory;
+  char* extension;
+  int im_width, im_height;
 
   static struct poptOption options[] = {
-    { "img_width",'w',POPT_ARG_INT,&im_width_arg,0,"Image width","NUM" },
-    { "img_height",'h',POPT_ARG_INT,&im_height_arg,0,"Image height","NUM" },
-    { "imgs_directory",'d',POPT_ARG_STRING,&imgs_directory_arg,0,"Directory to save images in","STR" },
-    { "extension",'e',POPT_ARG_STRING,&extension_arg,0,"Image extension","STR" },
+    { "img_width",'w',POPT_ARG_INT,&im_width,0,"Image width","NUM" },
+    { "img_height",'h',POPT_ARG_INT,&im_height,0,"Image height","NUM" },
+    { "imgs_directory",'d',POPT_ARG_STRING,&imgs_directory,0,"Directory to save images in","STR" },
+    { "extension",'e',POPT_ARG_STRING,&extension,0,"Image extension","STR" },
     POPT_AUTOHELP
     { NULL, 0, 0, NULL, 0, NULL, NULL }
   };
@@ -57,34 +28,25 @@ int main(int argc, char const *argv[])
   POpt popt(NULL, argc, argv, options, 0);
   int c;
   while((c = popt.getNextOpt()) >= 0) {}
-  
-  cap1.set(CAP_PROP_BUFFERSIZE, 2);
-  cap2.set(CAP_PROP_BUFFERSIZE, 2);
 
-  std::thread thread1(cam1, imgs_directory_arg, extension_arg, im_width_arg, im_height_arg); 
-  std::thread thread2(cam2, imgs_directory_arg, extension_arg, im_width_arg, im_height_arg);
-
-  while(1){
-    m1.lock();
-    img_res1 = img1;
-    m1.unlock();
-    m2.lock();
-    img_res2 = img2;
-    m2.unlock();
+  VideoCapture cap1("rtsp://192.168.1.171");
+  VideoCapture cap2("rtsp://192.168.1.171");
+  Mat img1, img_res1, img2, img_res2;
+  while (1) {
+    cap1 >> img1;
+    cap2 >> img2;
+    resize(img1, img_res1, Size(im_width, im_height));
+    resize(img2, img_res2, Size(im_width, im_height));
     imshow("IMG1", img_res1);
     imshow("IMG2", img_res2);
-    int key = waitKey(50);
-    if ((key != 255)) {
+    if (waitKey(30) > 0) {
       x++;
       char filename1[200], filename2[200];
-      sprintf(filename1, "%sright%d.%s", imgs_directory_arg, x, extension_arg);
-      sprintf(filename2, "%sleft%d.%s", imgs_directory_arg, x, extension_arg);
-      cout << "Saving img pair" << x << endl;
+      sprintf(filename1, "%sleft%d.%s", imgs_directory, x, extension);
+      sprintf(filename2, "%sright%d.%s", imgs_directory, x, extension);
+      cout << "Saving img pair " << x << endl;
       imwrite(filename1, img_res1);
       imwrite(filename2, img_res2);
-      t = true;
-    } else if (key == 255){
-      t = false;
     }
   }
   return 0;
