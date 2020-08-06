7f96337333160157dc8f3bcbc7663c4fc3be072f
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-07 00:39:18 +0200

    Update read_images.cpp

--- robot_ws/src/nsra_computer_vision/src/read_images.cpp
+++ robot_ws/src/nsra_computer_vision/src/read_images.cpp
@@ -4,16 +4,30 @@
 #include <stdio.h>
 #include <iostream>
 #include "popt_pp.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
 
 using namespace std;
 using namespace cv;
 
 int x = 0;
 
+/*
+void imgSaveCallback(const std_msgs::String::ConstPtr& msg) {
+  x++;
+  char filename1[200];
+  sprintf(filename1, "%sleft%d.%s", imgs_directory, x, extension);
+  cout << "Saving img pair " << x << endl;
+  imwrite(filename1, img1);
+}
+*/
+
 int main(int argc, char const *argv[])
 {
   char* imgs_directory;
   char* extension;
+  char* cam_address;
   int im_width, im_height;
 
   static struct poptOption options[] = {
@@ -21,6 +35,7 @@ int main(int argc, char const *argv[])
     { "img_height",'h',POPT_ARG_INT,&im_height,0,"Image height","NUM" },
     { "imgs_directory",'d',POPT_ARG_STRING,&imgs_directory,0,"Directory to save images in","STR" },
     { "extension",'e',POPT_ARG_STRING,&extension,0,"Image extension","STR" },
+    { "cam_address",'c',POPT_ARG_STRING,&cam_address,0,"RTSP Camera Address","STR" },
     POPT_AUTOHELP
     { NULL, 0, 0, NULL, 0, NULL, NULL }
   };
@@ -29,25 +44,23 @@ int main(int argc, char const *argv[])
   int c;
   while((c = popt.getNextOpt()) >= 0) {}
 
-  VideoCapture cap1("rtsp://192.168.1.171");
-  VideoCapture cap2("rtsp://192.168.1.102");
-  Mat img1, img_res1, img2, img_res2;
+  //const char *const_name = name;
+  //ros::init(argc, argv, const_name);
+  //ros::NodeHandle n;
+
+  //ros::Subscriber sub = n.subscribe("save_img", 1000, imgSaveCallback);
+
+  VideoCapture cap1(cam_address);
+
+  cap1.set(CAP_PROP_BUFFERSIZE, 2);
+  //ros::Rate r(100);
+  Mat img1, img_res1;
   while (1) {
     cap1 >> img1;
-    cap2 >> img2;
     resize(img1, img_res1, Size(im_width, im_height));
-    resize(img2, img_res2, Size(im_width, im_height));
-    imshow("IMG1", img_res1);
-    imshow("IMG2", img_res2);
-    if (waitKey(30) > 0) {
-      x++;
-      char filename1[200], filename2[200];
-      sprintf(filename1, "%sleft%d.%s", imgs_directory, x, extension);
-      sprintf(filename2, "%sright%d.%s", imgs_directory, x, extension);
-      cout << "Saving img pair " << x << endl;
-      imwrite(filename1, img_res1);
-      imwrite(filename2, img_res2);
-    }
+    imshow(name, img_res1);
+    //ros::spinOnce();
+    //r.sleep();
   }
   return 0;
 }
