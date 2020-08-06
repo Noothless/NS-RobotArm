a1582e8b314a6b4b2928f00950c8c3bdc76939a3
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-07 00:05:44 +0200

    Update read_images.cpp

--- robot_ws/src/nsra_computer_vision/src/read_images.cpp
+++ robot_ws/src/nsra_computer_vision/src/read_images.cpp
@@ -45,7 +45,7 @@ int main(int argc, char const *argv[])
   while((c = popt.getNextOpt()) >= 0) {}
 
   char *const_name = (char *)"node";
-  const string name2 = "test";
+  const string name2 = (char *)"test";
   ros::init(argc, argv, name2);
   ros::NodeHandle n;
 
