b1e2471f528266215b6c2c2c4950e1532e746cc0
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-08 17:28:48 +0200

    srv

---
+++ robot_ws/src/nsra_computer_vision/src/bottle_detection.cpp
@@ -0,0 +1,31 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <iostream>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+#include "nsra_computer_vision/2d_coords.srv"
+
+using namespace std;
+using namespace cv;
+
+Mat pnts3D(1,N,CV_64FC4);
+Mat cam0pnts(1,N,CV_64FC2);
+Mat cam1pnts(1,N,CV_64FC2);
+Mat P1, P2;
+
+int main(int argc, char** argv)
+{
+
+    ros::init(argc, argv, "bottle_detection");
+    ros::NodeHandle n;
+
+    FileStorage fs(ros::package::getPath("nsra_computer_vision") + "/" + "cam_stereo.yml", FileStorage::READ);
+
+    fs["P1"] >> P1;
+    fs["P2"] >> P2;
+
+
+}
