5cb58e480ed09e2b8c1be41e2a8267d32303217e
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-09 00:16:27 +0200

    opencv

--- robot_ws/src/nsra_computer_vision/src/bottle_detection.cpp
+++ robot_ws/src/nsra_computer_vision/src/bottle_detection.cpp
@@ -32,25 +32,34 @@ int main(int argc, char** argv)
     cout << P1 << endl;
     cout << P2 << endl;
 
-    cam0pnts.at<double>(0,0) = 663.07874;
-    cam0pnts.at<double>(0,1) = 698.88965;
-    cam1pnts.at<double>(0,0) = 1176.7567;
-    cam1pnts.at<double>(0,1) = 732.3928;
+    vector<vector<double>> vec{ {763.9179 , 146.99612, 1136.4603, 189.02242},
+                                {1410.4741, 178.00208, 1789.9531, 202.65692},
+                                {662.7758 , 640.4207 , 1161.9884, 684.38464},
+                                {1211.7473, 679.71954, 1729.7555, 708.58624} };
+
+
+
+    for(int i = 0; i < 4; i++) {
+        cam0pnts.at<double>(0,0) = vec[i][0];
+        cam0pnts.at<double>(0,1) = vec[i][1];
+        cam1pnts.at<double>(0,0) = vec[i][2];
+        cam1pnts.at<double>(0,1) = vec[i][3];
     
-    triangulatePoints(P1,P2,cam1pnts,cam0pnts,pnts3D);
+        triangulatePoints(P1,P2,cam1pnts,cam0pnts,pnts3D);
 
-    cout << pnts3D << endl;
+        //cout << pnts3D << endl;
 
-    cv::Mat1f Thomogeneous(4, 1); 
-    Thomogeneous(0) = pnts3D.at<double>(0,0);
-    Thomogeneous(1) = pnts3D.at<double>(0,1);
-    Thomogeneous(2) = pnts3D.at<double>(0,2);
-    Thomogeneous(3) = pnts3D.at<double>(0,3);
+        cv::Mat1f Thomogeneous(4, 1); 
+        Thomogeneous(0) = pnts3D.at<double>(0,0);
+        Thomogeneous(1) = pnts3D.at<double>(0,1);
+        Thomogeneous(2) = pnts3D.at<double>(0,2);
+        Thomogeneous(3) = pnts3D.at<double>(0,3);
 
-    Mat Th = Thomogeneous.reshape(4);
+        Mat Th = Thomogeneous.reshape(4);
 
-    cv::Mat T;
-    cv::convertPointsFromHomogeneous(Th, T);
+        cv::Mat T;
+        cv::convertPointsFromHomogeneous(Th, T);
 
-    cout << T << endl;
+        cout << T << endl;
+    }
 }
