260835e4207d97a5affc9ad2b2608f94fc5c1b82
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-08 22:16:43 +0200

    opencv

--- robot_ws/src/nsra_computer_vision/src/bottle_detection.cpp
+++ robot_ws/src/nsra_computer_vision/src/bottle_detection.cpp
@@ -32,13 +32,10 @@ int main(int argc, char** argv)
     cout << P1 << endl;
     cout << P2 << endl;
 
-    double c0[] = {876.5602,474.18985};
-    double c1[] = {1381.2324,483.25412};
-
-    cam0pnts.at<double>(0,0) = 1034.2808;
-    cam0pnts.at<double>(0,1) = 487.31534;
-    cam1pnts.at<double>(0,0) = 1499.1863;
-    cam1pnts.at<double>(0,1) = 507.90436;
+    cam0pnts.at<double>(0,0) = 1175.1108;
+    cam0pnts.at<double>(0,1) = 198.12099;
+    cam1pnts.at<double>(0,0) = 1565.3248;
+    cam1pnts.at<double>(0,1) = 216.99852;
     
     triangulatePoints(P1,P2,cam1pnts,cam0pnts,pnts3D);
 
