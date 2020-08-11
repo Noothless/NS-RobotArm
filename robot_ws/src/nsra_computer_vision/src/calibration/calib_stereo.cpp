eabbb3312fccad434d2500c17740c7896b10252d
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-11 10:42:44 +0200

    Update calib_stereo.cpp

--- robot_ws/src/nsra_computer_vision/src/calibration/calib_stereo.cpp
+++ robot_ws/src/nsra_computer_vision/src/calibration/calib_stereo.cpp
@@ -113,10 +113,11 @@ int main(int argc, char const *argv[])
   while((c = popt.getNextOpt()) >= 0) {}
   
   string img_dir = ros::package::getPath("nsra_computer_vision") + "/images/";
-  string calib_dir = ros::package::getPath("nsra_computer_vision") + "/config/";
+  string calib_dir_left = ros::package::getPath("nsra_computer_vision") + "/config/" + leftcalib_file;
+  string calib_dir_right = ros::package::getPath("nsra_computer_vision") + "/config/" + rightcalib_file;
 
-  FileStorage fsl(calib_dir.c_str() + leftcalib_file, FileStorage::READ);
-  FileStorage fsr(calib_dir.c_str() + rightcalib_file, FileStorage::READ);
+  FileStorage fsl(calib_dir.c_str(), FileStorage::READ);
+  FileStorage fsr(calib_dir.c_str(), FileStorage::READ);
 
   load_image_points(9, 6, num_imgs, fsl["square_Size"],
                    img_dir.c_str(), img_dir.c_str(), leftimg_filename, rightimg_filename, extension);
