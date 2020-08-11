456ae28dc0c0bf7ff9cbecac25403e901051c91f
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-08-11 21:38:23 +0200

    Update single_cam_callibration.cpp

--- robot_ws/src/nsra_computer_vision/src/deprecated/single_cam_callibration.cpp
+++ robot_ws/src/nsra_computer_vision/src/deprecated/single_cam_callibration.cpp
@@ -96,7 +96,7 @@ public:
         else
         {
             inputCapture.open(input);
-            inputCapture.set(CAP_PROP_BUFFERSIZE, 2);
+            //inputCapture.set(CAP_PROP_BUFFERSIZE, 2);
         }
         if (inputType == INVALID)
         {
