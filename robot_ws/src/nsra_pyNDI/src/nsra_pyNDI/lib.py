891a1f6d867f57fea18a69e4c2b860e8aa16e7af
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-12-25 19:55:22 +0100

    py_ndi

--- robot_ws/src/nsra_pyNDI/src/nsra_pyNDI/lib.py
+++ robot_ws/src/nsra_pyNDI/src/nsra_pyNDI/lib.py
@@ -1,6 +1,10 @@
 import sys
 import os.path
 from cffi import FFI
+import rospkg
+
+rospack = rospkg.RosPack()
+pack_path = rospack.get_path('nsra_pyNDI')
 
 ffi = FFI()
 
@@ -147,7 +151,7 @@ ffi.cdef(r"""
 basedir = os.path.dirname(__file__)
 arch = 'x64' if sys.maxsize > 2**32 else 'x86'
 #lib = ffi.dlopen(os.path.join("~/", "bin", f"Processing.NDI.Lib.{arch}.dll"))
-lib = ffi.dlopen("/home/noa/bin/libndi.so.4.5.3")
+lib = ffi.dlopen(pack_path + "/bin/libndi.so.4.5.3")
 
 
 if not lib.NDIlib_initialize():
