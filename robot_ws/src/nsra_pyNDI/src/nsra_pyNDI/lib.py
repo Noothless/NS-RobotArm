68e0acaa969ab2a7fb96e1a810d080b6a40bf2d4
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-12-25 19:46:02 +0100

    Update lib.py

--- robot_ws/src/nsra_pyNDI/src/nsra_pyNDI/lib.py
+++ robot_ws/src/nsra_pyNDI/src/nsra_pyNDI/lib.py
@@ -148,7 +148,7 @@ basedir = os.path.dirname(__file__)
 arch = 'x64' if sys.maxsize > 2**32 else 'x86'
 print("suck my dick")
 #lib = ffi.dlopen(os.path.join("~/", "bin", f"Processing.NDI.Lib.{arch}.dll"))
-lib = ffi.dlopen("/home/noa/bin/Processing.NDI.Lib.x64.dll")
+lib = ffi.dlopen("/home/noa/bin/libndi.so.4.5.3")
 
 
 if not lib.NDIlib_initialize():
