a515c06cae2556593fcfc7240d018bb953c7d3af
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2020-12-29 17:54:58 +0100

    Update nsra_bottle_detection.cpp

--- robot_ws/src/nsra_robot_vision/src/nsra_bottle_detection.cpp
+++ robot_ws/src/nsra_robot_vision/src/nsra_bottle_detection.cpp
@@ -87,7 +87,7 @@ class Camera
             if (video_frame.p_data)
 		    {
                 cout << "test4" << endl;
-                cv::Mat ret_frame(video_frame.yres, video_frame.xres, CV_8UC4, (uint8_t *)video_frame.p_data);
+                cv::Mat ret_frame(video_frame.yres, video_frame.xres, CV_8UC3, (uint8_t *)video_frame.p_data);
                 cout << "test5" << endl;
                 //cv::Mat out;
                 //cvtColor(ret_frame, out, cv::COLOR_RGBA2BGR);
