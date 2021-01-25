0c6639e1952794b80e07c9301ca8ca250d90cc83
Noothless <noa.sendlhofer@gmail.com>
2021-01-25 19:38:54 +0100

    Update nsra_hw_interface.cpp

--- robot_ws/src/nsra_controller/nsra_control/src/nsra_hw_interface.cpp
+++ robot_ws/src/nsra_controller/nsra_control/src/nsra_hw_interface.cpp
@@ -62,7 +62,7 @@ NSRAHWInterface::NSRAHWInterface(ros::NodeHandle &nh, urdf::Model *urdf_model)
 
   try
   {
-    serial_stream.Open("/dev/controller_lol");
+    serial_stream.Open("/dev/ttyUSB0");
     ROS_INFO_NAMED("nsra_hardware_interface", "Connected!");
   }
   catch (char *excp)
