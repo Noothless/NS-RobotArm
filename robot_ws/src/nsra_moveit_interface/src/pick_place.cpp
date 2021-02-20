1f08f9e9a5542bb9cb3ebbdffc4a168d457ed71c
Noothless <noa.sendlhofer@gmail.com>
2021-02-20 12:59:09 +0100

    Update pick_place.cpp

--- robot_ws/src/nsra_moveit_interface/src/pick_place.cpp
+++ robot_ws/src/nsra_moveit_interface/src/pick_place.cpp
@@ -102,7 +102,7 @@ void pick(moveit::planning_interface::MoveGroupInterface& move_group)
   tf2::Quaternion orientation;
   orientation.setRPY(M_PI / 2, 0, 0);
   grasps[0].grasp_pose.pose.orientation = tf2::toMsg(orientation);
-  grasps[0].grasp_pose.pose.position.x = 0.3;
+  grasps[0].grasp_pose.pose.position.x = 0.35;
   grasps[0].grasp_pose.pose.position.y = 0;
   grasps[0].grasp_pose.pose.position.z = 0.5;
 
