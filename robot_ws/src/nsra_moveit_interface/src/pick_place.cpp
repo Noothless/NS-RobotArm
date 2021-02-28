7f665ec86abdcc2c82d96ae2a7f43b501a26b5da
Noothless <noa.sendlhofer@gmail.com>
2021-02-28 22:52:08 +0100

    odrive

--- robot_ws/src/nsra_moveit_interface/src/pick_place.cpp
+++ robot_ws/src/nsra_moveit_interface/src/pick_place.cpp
@@ -1,103 +1,81 @@
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2012, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
+/*****************************************************************************
+*  BSD 3-Clause License
+* 
+*  Copyright (c) 2020, Noa Sendlhofer
+*  All rights reserved.
+* 
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions are met:
+* 
+*  1. Redistributions of source code must retain the above copyright notice, this
+*     list of conditions and the following disclaimer.
+* 
+*  2. Redistributions in binary form must reproduce the above copyright notice,
+*     this list of conditions and the following disclaimer in the documentation
+*     and/or other materials provided with the distribution.
+* 
+*  3. Neither the name of the copyright holder nor the names of its
+*     contributors may be used to endorse or promote products derived from
+*     this software without specific prior written permission.
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+*  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+*  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+*  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+*  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+****************************************************************************/
 
-/* Author: Ioan Sucan, Ridhwan Luthra*/
+/* Author: Noa Sendlhofer
+   Desc:   Part of the NSRA Robot Vision stack. Bottle detection MoveIt! Interface.
+*/
 
-// ROS
+
 #include <ros/ros.h>
-
-// MoveIt
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit/move_group_interface/move_group_interface.h>
-
-// TF2
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 void openGripper(trajectory_msgs::JointTrajectory& posture)
 {
-  // BEGIN_SUB_TUTORIAL open_gripper
-  /* Add both finger joints of panda robot. */
-  
+
   posture.joint_names.resize(2);
   posture.joint_names[0] = "Slider8";
   posture.joint_names[1] = "Slider9";
-  
-  /* Set them as open, wide enough for the object to fit. */
   
   posture.points.resize(1);
   posture.points[0].positions.resize(2);
   posture.points[0].positions[0] = 0.02;
   posture.points[0].positions[1] = 0.02;
   posture.points[0].time_from_start = ros::Duration(0.5);
-  // END_SUB_TUTORIAL
   
 }
 
 void closedGripper(trajectory_msgs::JointTrajectory& posture)
 {
-  // BEGIN_SUB_TUTORIAL closed_gripper
-  /* Add both finger joints of panda robot. */
-  
+
   posture.joint_names.resize(2);
   posture.joint_names[0] = "Slider8";
   posture.joint_names[1] = "Slider9";
-  
-  /* Set them as closed. */
-  
+
   posture.points.resize(1);
   posture.points[0].positions.resize(2);
   posture.points[0].positions[0] = 0.00;
   posture.points[0].positions[1] = 0.00;
   posture.points[0].time_from_start = ros::Duration(0.5);
   
-  // END_SUB_TUTORIAL
 }
 
 void pick(moveit::planning_interface::MoveGroupInterface& move_group)
 {
-  // BEGIN_SUB_TUTORIAL pick1
-  // Create a vector of grasps to be attempted, currently only creating single grasp.
-  // This is essentially useful when using a grasp generator to generate and test multiple grasps.
+
   std::vector<moveit_msgs::Grasp> grasps;
   grasps.resize(1);
 
-  // Setting grasp pose
-  // ++++++++++++++++++++++
-  // This is the pose of panda_link8. |br|
-  // Make sure that when you set the grasp_pose, you are setting it to be the pose of the last link in
-  // your manipulator which in this case would be `"panda_link8"` You will have to compensate for the
-  // transform from `"panda_link8"` to the palm of the end effector.
   grasps[0].grasp_pose.header.frame_id = "world";
   tf2::Quaternion orientation;
   orientation.setRPY(M_PI / 2, 0, 0);
@@ -106,100 +84,59 @@ void pick(moveit::planning_interface::MoveGroupInterface& move_group)
   grasps[0].grasp_pose.pose.position.y = 0;
   grasps[0].grasp_pose.pose.position.z = 0.5;
 
-  // Setting pre-grasp approach
-  // ++++++++++++++++++++++++++
-  /* Defined with respect to frame_id */
+
   grasps[0].pre_grasp_approach.direction.header.frame_id = "world";
-  /* Direction is set as positive x axis */
   grasps[0].pre_grasp_approach.direction.vector.x = 1.0;
   grasps[0].pre_grasp_approach.min_distance = 0.095;
   grasps[0].pre_grasp_approach.desired_distance = 0.115;
 
-  // Setting post-grasp retreat
-  // ++++++++++++++++++++++++++
-  /* Defined with respect to frame_id */
+
   grasps[0].post_grasp_retreat.direction.header.frame_id = "world";
-  /* Direction is set as positive z axis */
   grasps[0].post_grasp_retreat.direction.vector.z = 1.0;
   grasps[0].post_grasp_retreat.min_distance = 0.1;
   grasps[0].post_grasp_retreat.desired_distance = 0.25;
 
-  // Setting posture of eef before grasp
-  // +++++++++++++++++++++++++++++++++++
+
   openGripper(grasps[0].pre_grasp_posture);
-  // END_SUB_TUTORIAL
-
-  // BEGIN_SUB_TUTORIAL pick2
-  // Setting posture of eef during grasp
-  // +++++++++++++++++++++++++++++++++++
   closedGripper(grasps[0].grasp_posture);
-  // END_SUB_TUTORIAL
 
-  // BEGIN_SUB_TUTORIAL pick3
-  // Set support surface as table1.
   move_group.setSupportSurfaceName("table1");
-  // Call pick to pick up the object using the grasps given
-  move_group.setMaxVelocityScalingFactor(0.6);
-  move_group.setMaxAccelerationScalingFactor(0.3);
 
   move_group.pick("object", grasps);
-  // END_SUB_TUTORIAL
+
 }
 
 void place(moveit::planning_interface::MoveGroupInterface& group)
 {
-  // BEGIN_SUB_TUTORIAL place
-  // TODO(@ridhwanluthra) - Calling place function may lead to "All supplied place locations failed. Retrying last
-  // location in verbose mode." This is a known issue. |br|
-  // |br|
-  // Ideally, you would create a vector of place locations to be attempted although in this example, we only create
-  // a single place location.
+
   std::vector<moveit_msgs::PlaceLocation> place_location;
   place_location.resize(1);
 
-  // Setting place location pose
-  // +++++++++++++++++++++++++++
   place_location[0].place_pose.header.frame_id = "world";
   tf2::Quaternion orientation;
+
   orientation.setRPY(0, 0, M_PI / 2);
   place_location[0].place_pose.pose.orientation = tf2::toMsg(orientation);
-
-  /* For place location, we set the value to the exact location of the center of the object. */
   place_location[0].place_pose.pose.position.x = 0;
   place_location[0].place_pose.pose.position.y = 0.8;
   place_location[0].place_pose.pose.position.z = 0.5;
 
-  // Setting pre-place approach
-  // ++++++++++++++++++++++++++
-  /* Defined with respect to frame_id */
   place_location[0].pre_place_approach.direction.header.frame_id = "world";
-  /* Direction is set as negative z axis */
   place_location[0].pre_place_approach.direction.vector.z = -1.0;
   place_location[0].pre_place_approach.min_distance = 0.095;
   place_location[0].pre_place_approach.desired_distance = 0.115;
 
-  // Setting post-grasp retreat
-  // ++++++++++++++++++++++++++
-  /* Defined with respect to frame_id */
   place_location[0].post_place_retreat.direction.header.frame_id = "world";
-  /* Direction is set as negative y axis */
   place_location[0].post_place_retreat.direction.vector.y = -1.0;
   place_location[0].post_place_retreat.min_distance = 0.1;
   place_location[0].post_place_retreat.desired_distance = 0.25;
 
-  // Setting posture of eef after placing object
-  // +++++++++++++++++++++++++++++++++++++++++++
-  /* Similar to the pick case */
   openGripper(place_location[0].post_place_posture);
 
-  // Set support surface as table2.
   group.setSupportSurfaceName("table2");
-  // Call place to place the object using the place locations given.
-  group.setMaxVelocityScalingFactor(0.6);
-  group.setMaxAccelerationScalingFactor(0.3);
 
   group.place("object", place_location);
-  // END_SUB_TUTORIAL
+
 }
 
 void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& planning_scene_interface)
@@ -285,7 +222,7 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "test_pick_place");
+  ros::init(argc, argv, "nsra_moveit_interface");
   ros::NodeHandle nh;
   ros::AsyncSpinner spinner(1);
   spinner.start();
@@ -297,7 +234,6 @@ int main(int argc, char** argv)
   
   addCollisionObjects(planning_scene_interface);
 
-  // Wait a bit for ROS things to initialize
   ros::WallDuration(1.0).sleep();
 
   pick(group);
