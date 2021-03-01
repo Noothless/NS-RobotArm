b5f62300c06460cc330d39df3dc41f4fba4284b3
Noothless <noa.sendlhofer@gmail.com>
2021-03-01 21:42:32 +0100

    Update nsra_hw_interface.cpp

--- robot_ws/src/nsra_controller/nsra_control/src/nsra_hw_interface.cpp
+++ robot_ws/src/nsra_controller/nsra_control/src/nsra_hw_interface.cpp
@@ -108,7 +108,7 @@ void NSRAHWInterface::write(ros::Duration &elapsed_time)
     {
       msg.data = joint_position_command_[i]*1024000/pi/8192;
       drive_pub1.publish(msg);
-      steps = round(joint_position_command_[i]*12500/pi);
+      steps = round(joint_position_command_[i]*12500*0.885/pi);
       msg_step.axis1 = steps;
     } else if(i == 1)
     {
