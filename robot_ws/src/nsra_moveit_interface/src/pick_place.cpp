98c976f1bf625bda14b90cf3c749f65f149cb613
Noothless <noa.sendlhofer@gmail.com>
2021-03-27 17:15:33 +0100

    Update pick_place.cpp

--- robot_ws/src/nsra_moveit_interface/src/pick_place.cpp
+++ robot_ws/src/nsra_moveit_interface/src/pick_place.cpp
@@ -98,7 +98,7 @@ void empty_bottle(moveit::planning_interface::MoveGroupInterface& move_group, do
     myQuaternion.setRPY(current_state[0],current_state[1],current_state[2]);
     myQuaternion = myQuaternion.normalize();
 
-    geometry_msgs::Quaternion = quat;
+    geometry_msgs::Quaternion quat;
 
     quaternionTFToMsg(myQuaternion , quat);
 
