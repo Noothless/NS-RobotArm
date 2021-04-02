fc9f8b2d868b2e447ff83c63912c863e1b301d06
Noothless <noa.sendlhofer@gmail.com>
2021-04-02 12:18:18 +0200

    inp

--- robot_ws/src/nsra_moveit_interface/src/pick_place.cpp
+++ robot_ws/src/nsra_moveit_interface/src/pick_place.cpp
@@ -308,11 +308,13 @@ int main(int argc, char** argv)
       ros::WallDuration(1.0).sleep();
 
       empty_bottle(group, 300.0/1000, 500.0/1000, 500.0/1000, 0);
-      empty_bottle(group, 300.0/1000, 500.0/1000, 500.0/1000, 90);
+      empty_bottle(group, 300.0/1000, 500.0/1000, 500.0/1000, -90);
       ros::WallDuration(7.0).sleep();
-      empty_bottle(group, 300.0/1000, 500.0/1000, 500.0/1000, -45);
+      empty_bottle(group, 300.0/1000, 500.0/1000, 500.0/1000, 45);
 
       ros::WallDuration(1.0).sleep();
+
+      std::cin >> var;
 
       place(group, val);
 
